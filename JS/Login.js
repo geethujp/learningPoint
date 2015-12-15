5b5d0284c2c1448b060e0595b54690d7d58782b4
geethujp <geethuj@qburst.com>
2015-12-15 12:15:13 +0530

    Adding files

---
+++ JS/Login.js
@@ -0,0 +1,21 @@
+function validateLoginForm() {
+  
+     var a = document.forms["myLoginForm"]["username"].value;
+      var b = document.forms["myLoginForm"]["password"].value;
+     
+    if (a == null || a == "") {
+        alert("User name must be filled out");
+        return false;
+    }
+    
+    if (b == null || b == "") {
+        alert("Password must be filled out");
+        return false;
+    }
+   if(a==geethu&&b==123)
+   {
+    alert("Access Granted");
+    return true;
+   }
+  
+}
